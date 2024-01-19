bc106cfe31840c4d5b11858676b5b382e3cc386e
DimanYLT <150605065+DimanYLT@users.noreply.github.com>
2024-01-19 21:10:15 +0700

    Add files via upload

---
+++ 01_10_or_gate_using_mux.sv
@@ -0,0 +1,57 @@
+module mux
+(
+  input  d0, d1,
+  input  sel,
+  output y
+);
+
+  assign y = sel ? d1 : d0;
+
+endmodule
+
+//----------------------------------------------------------------------------
+
+module or_gate_using_mux
+(
+    input  a,
+    input  b,
+    output o
+);
+
+  mux imux (a, 1'b1, b, o);
+  
+endmodule
+
+//----------------------------------------------------------------------------
+
+module testbench;
+
+  logic a, b, o;
+  int i, j;
+
+  or_gate_using_mux inst (a, b, o);
+
+  initial
+    begin
+      for (i = 0; i <= 1; i++)
+      for (j = 0; j <= 1; j++)
+      begin
+        a = i;
+        b = j;
+
+        # 1;
+
+        $display ("TEST %b | %b = %b", a, b, o);
+
+        if (o !== (a | b))
+          begin
+            $display ("%s FAIL: %h EXPECTED", `__FILE__, a | b);
+            $finish;
+          end
+      end
+
+      $display ("%s PASS", `__FILE__);
+      $finish;
+    end
+
+endmodule
